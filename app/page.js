13816c2ca3b17c64231d5263bad0594a31ba78fe
DeepralShakya <shakyadeepral44@gmail.com>
2024-03-19 08:51:55 +0545

    adjustment

--- app/page.js
+++ app/page.js
@@ -1,11 +1,10 @@
 'use client'
 
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="py-4 text-center">
-      <h1 className="text-red-700">Deepral Shakya</h1>
+    <div className="py-4">
+      <h1 className="text-5xl text-center text-red-700">Deepral Shakya</h1>
+      <h1 className="text-5xl text-center text-red-700">Exam</h1>
     </div>
   );
 }
