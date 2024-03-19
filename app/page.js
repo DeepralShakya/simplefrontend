38a7007604761856608fe6bfbc600d907a975464
DeepralShakya <shakyadeepral44@gmail.com>
2024-03-19 09:08:42 +0545

    address added

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="py-4">
       <h1 className="text-5xl text-center text-red-700">Deepral Shakya</h1>
+      <h1 className="text-5xl text-center text-red-700">Imadol, Lalitpur</h1>
       <h1 className="text-5xl text-center text-red-700">Exam</h1>
     </div>
   );
